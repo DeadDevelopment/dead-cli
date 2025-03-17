0effa33ef6b6a89607f2361a31264ef562be3311
Jack <95598686+JackValdiviez@users.noreply.github.com>
2025-03-17 18:29:44 -0500

    centralized endpoints

---
+++ src/utils/endpoints.ts
@@ -0,0 +1,26 @@
+export const LOCAL_BASE_URL = "http://127.0.0.1:5001/deadlibrary-53c38/us-central1/"
+
+export const ENDPOINTS = {
+    button: `${LOCAL_BASE_URL}generateButton`,
+    card: `${LOCAL_BASE_URL}generateCard`,
+    componentList: `${LOCAL_BASE_URL}generateComponentList`,
+    formField: `${LOCAL_BASE_URL}generateFormField`,
+    formGroup: `${LOCAL_BASE_URL}generateFormGroup`,
+    gridList: `${LOCAL_BASE_URL}generateGridList`,
+    imageDiv: `${LOCAL_BASE_URL}generateImageDiv`,
+    loadingSpinner: `${LOCAL_BASE_URL}generateLoadingSpinner`,
+    menu: `${LOCAL_BASE_URL}generateMenu`,
+    mobileToolbar: `${LOCAL_BASE_URL}generateMobileToolbar`,
+    tabs: `${LOCAL_BASE_URL}generateTabs`,
+    theme: `${LOCAL_BASE_URL}generateTheme`,
+    titleButton: `${LOCAL_BASE_URL}generateTitleButton`,
+    titleList: `${LOCAL_BASE_URL}generateTitleList`,
+    titleParagraph: `${LOCAL_BASE_URL}generateTitleParagraph`,
+    titleParagraphButton: `${LOCAL_BASE_URL}generateTitleParagraphButton`,
+    titleSubList: `${LOCAL_BASE_URL}generateTitleSubList`,
+    titleSubListButton: `${LOCAL_BASE_URL}generateTitleSubListButton`,
+    titleSubParagraph: `${LOCAL_BASE_URL}generateTitleSubParagraph`,
+    titleSubParagraphButton: `{LOCAL_BASE_URL}generateTitleSubParagraphButton`,
+    toolbar: `${LOCAL_BASE_URL}generateToolbar`,
+}
+
