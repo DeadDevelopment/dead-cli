87324d2a46f2ba710b9d1dd7f864012b9ee1a708
Jack <95598686+JackValdiviez@users.noreply.github.com>
2025-03-23 18:42:17 -0500

    generateIconParagraph cmd added

---
+++ src/commands/generateIconParagraph.ts
@@ -0,0 +1,35 @@
+import { Command } from 'commander';
+import { generateIp } from '../handlers/IconParagraph';
+
+export function generateIconParagraph(program: Command) {
+    program
+        .command('generateIconParagraph')
+        .alias('ip')
+        .description('Generate an IconParagraph component.')
+        .option('-n, --name <name>', 'Name your files.', 'dead-icon-paragraph')
+        .option(
+            '-i, --icon <icon>',
+            'Icon config',
+            value => JSON.stringify(value),
+            JSON.stringify({
+                ariaLabel: "Skull Icon.",
+                fontIcon: "Skull",
+                classes: "m-1rem body"
+            })
+        )
+        .option(
+            '-p, --paragraph <paragraph>',
+            'Add a paragraph option with a text and classes property each a string.',
+            value => JSON.parse(value),
+            JSON.stringify({
+                text: "Paragraph element.", classes: "m-1rem body pri-dark"
+            })
+        )
+        .action(async (options) => {
+            try {
+                await generateIp(options);
+            } catch (error: any) {
+                console.error('Error generating code:', error.message);
+            }
+        });
+}
