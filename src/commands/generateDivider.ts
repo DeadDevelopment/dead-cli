865b8f1c5aefc71b0ee0eef6fe89e22241dcbf26
Jack <95598686+JackValdiviez@users.noreply.github.com>
2025-03-24 21:32:41 -0500

    added init cmd

---
+++ src/commands/generateDivider.ts
@@ -0,0 +1,20 @@
+import { Command } from 'commander';
+import { generatedivider } from '../handlers/Divider';
+
+export function generateDivider(program: Command) {
+    program
+    .command('generateDivider')
+    .alias('d')
+    .description('Generate a divider component.')
+    .option('-n, --name <name>', 'Name of the files.', 'dead-divider')
+    .option('-o, --orientation <orientation>', 'Vertical true/false', 'false')
+    .option('-c, --class', 'Set classes', 'm-1rem pri')
+    .action(async (options) => {
+        try {
+            await generatedivider(options);
+        } catch (error: any) {
+            console.error('Error generating code:', error.message);
+        }
+    });
+}
+
