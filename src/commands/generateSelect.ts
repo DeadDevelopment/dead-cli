2dd9e1ea6c75d4480a03ef79d1af942cb0e45b42
Jack <95598686+JackValdiviez@users.noreply.github.com>
2025-03-26 23:17:37 -0500

    s

--- src/commands/generateSelect.ts
+++ src/commands/generateSelect.ts
@@ -11,23 +11,23 @@ export function generateSelect(program: Command) {
             '-f, --field <field>',
             'JSON field config',
             value => JSON.parse(value),
-            JSON.stringify({"appearance": "outline", "classes": "m-1rem", "required": "required"})
+            JSON.stringify({appearance: "outline", classes: "m-1rem", required: "required"})
         )
         .option(
             '-s, --select <select>',
             'JSON select config',
             value => JSON.parse(value),
-            JSON.stringify({"lClasses": "pri", "label": "Select an option"})
+            JSON.stringify({lClasses: "pri", label: "Select an option"})
         )
         .option (
             '-o, --options <options>',
             'JSON array of options',
             value => JSON.parse(value),
             JSON.stringify([
-                {"label": "Option 1", "value": "option1"},
-                {"label": "Option 2", "value": "option2"},
-                {"label": "Option 3", "value": "option3"},
-                {"label": "Option 4", "value": "option4"}
+                {label: "Option 1", value: "option1"},
+                {label: "Option 2", value: "option2"},
+                {label: "Option 3", value: "option3"},
+                {label: "Option 4", value: "option4"}
             ])
         )
         .action(async (options) => {
