44f664773b71d46e4629bcfd209236015d12e132
Jack <95598686+JackValdiviez@users.noreply.github.com>
2025-03-26 23:17:24 -0500

    lbar

--- src/commands/generateLoadingBar.ts
+++ src/commands/generateLoadingBar.ts
@@ -4,7 +4,7 @@ import { generateLbar } from '../handlers/LoadingBar';
 export function generateLoadingBar(program: Command) {
     program
         .command('generateLoadingBar')
-        .alias('lb')
+        .alias('lbar')
         .description('Generate a loading bar component.')
         .option('-n, --name <name>', 'Name your files.', 'dead-loading-bar')
         .option('-m, --mode <mode>', 'Set the mode of the loading bar.', 'buffer')
