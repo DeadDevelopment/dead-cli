044ece10fcc0e14dee828b59f59f3d7fbb47fc45
Jack <95598686+JackValdiviez@users.noreply.github.com>
2025-03-26 23:16:22 -0500

    ic

--- src/commands/generateImageCarousel.ts
+++ src/commands/generateImageCarousel.ts
@@ -20,14 +20,14 @@ export function generateImageCarousel(program: Command) {
             value => JSON.parse(value),
             JSON.stringify([
                 {
-                    "alt": "image 1",
-                    "src": "https://firebasestorage.googleapis.com/v0/b/deaddevelopment-95591.appspot.com/o/dead_skull.PNG?alt=media&token=1634c2ba-f3e8-49db-bfd6-0b49576cba4a",
-                    "classes": "m-1rem skull-logo" 
+                    alt: "image 1",
+                    src: "https://firebasestorage.googleapis.com/v0/b/deaddevelopment-95591.appspot.com/o/dead_skull.PNG?alt=media&token=1634c2ba-f3e8-49db-bfd6-0b49576cba4a",
+                    classes: "m-1rem skull-logo" 
                 },
                 {
-                    "alt": "image 2",
-                    "src": "https://firebasestorage.googleapis.com/v0/b/deaddevelopment-95591.appspot.com/o/dead_skull.PNG?alt=media&token=1634c2ba-f3e8-49db-bfd6-0b49576cba4a",
-                    "classes": "m-1rem skull-logo"
+                    alt: "image 2",
+                    src: "https://firebasestorage.googleapis.com/v0/b/deaddevelopment-95591.appspot.com/o/dead_skull.PNG?alt=media&token=1634c2ba-f3e8-49db-bfd6-0b49576cba4a",
+                    classes: "m-1rem skull-logo"
                 }
             ])
         )
@@ -35,19 +35,19 @@ export function generateImageCarousel(program: Command) {
             '-c, --controls <controls>',
             'carousel controls container config',
             value => JSON.parse(value),
-            JSON.stringify({"bclasses": "m-0-5rem", "btype": "mat-mini-fab", "classes": "m-1rem controls"})
+            JSON.stringify({bclasses: "m-0-5rem", btype: "mat-mini-fab", classes: "m-1rem controls"})
         )
         .option(
             '-p, --pIcon <pIcon>',
             'Config the previous image icon',
             value => JSON.parse(value),
-            JSON.stringify({"ariaLabel": "Previous image chevron.", "fontIcon": "chevron_left", "classes": "previous"})
+            JSON.stringify({ariaLabel: "Previous image chevron.", fontIcon: "chevron_left", classes: "previous"})
         )
         .option(
-            '-n, --nIcon <nIcon>',
+            '--ni, --nIcon <nIcon>',
             'Config the next image icon',
             value => JSON.parse(value),
-            JSON.stringify({"ariaLabel": "Next image chevron.", "fontIcon": "chevron_right", "classes": "next"})
+            JSON.stringify({ariaLabel: "Next image chevron.", fontIcon: "chevron_right", classes: "next"})
         )
         .action(async (options) => {
             try {
